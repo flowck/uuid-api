35873bcb4a9ccd52e40932aad6763d6a6999f6a4
Firmino Changani <flowck96@gmail.com>
2018-12-28 20:39:21 +0100

    fix issue with the port definition

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require("http");
 const app = require("./application");
 
-const PORT = 4000 || process.env.PORT;
+const PORT = process.env.PORT || 4000;
 http.createServer(app).listen(PORT, () => {
   console.log("The UUID server is up and running");
 });
