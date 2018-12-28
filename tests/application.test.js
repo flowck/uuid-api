57aa8e8812b77677e249a6788289d4e4c273fc08
Firmino Changani <flowck96@gmail.com>
2018-12-28 19:14:16 +0100

    Add a test case for /api/:n

--- tests/application.test.js
+++ tests/application.test.js
@@ -16,8 +16,8 @@ describe("UUIDs", () => {
   /*
    * Get a single uuid
    */
-  describe("/GET single uuid", () => {
-    it("It should GET a single uuid", done => {
+  describe("/GET single UUID", () => {
+    it("It should GET a single UUID", done => {
       chai
         .request(application)
         .get("/api/")
@@ -25,6 +25,20 @@ describe("UUIDs", () => {
           if (err) throw err;
           res.should.have.status(200);
           res.body.uuid.should.be.a.uuid("v4");
+          done();
+        });
+    });
+
+    it("It should GET 10 UUIDs", done => {
+      chai
+        .request(application)
+        .get("/api/10")
+        .end((err, res) => {
+          if (err) throw err;
+          res.should.have.status(200);
+          res.body.uuids.should.be.a("array");
+          res.body.uuids.length.should.be.eql(10);
+          res.body.uuids.forEach(result => result.uuid.should.be.a.uuid("v4"));
           done();
         });
     });
